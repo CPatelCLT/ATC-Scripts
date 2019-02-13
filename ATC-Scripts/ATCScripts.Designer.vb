4daa1884f0d5f7e10d6bbb5c72b7ba50205c9feb
Chirag <cpatel36@uncc.edu>
2019-02-13 02:13:20 -0500

    Basic functionality done

--- ATC-Scripts/ATCScripts.Designer.vb
+++ ATC-Scripts/ATCScripts.Designer.vb
@@ -30,13 +30,20 @@ Partial Class ATCScripts
         Dim TreeNode6 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("No SID Amended")
         Dim TreeNode7 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("No SID Amended Route & FL")
         Dim TreeNode8 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("IFR Clearances", New System.Windows.Forms.TreeNode() {TreeNode1, TreeNode2, TreeNode3, TreeNode4, TreeNode5, TreeNode6, TreeNode7})
-        Me.TreeView1 = New System.Windows.Forms.TreeView()
+        Dim TreeNode9 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("IFR Taxi")
+        Dim TreeNode10 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("VFRTaxi")
+        Dim TreeNode11 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("Taxi Instructions", New System.Windows.Forms.TreeNode() {TreeNode9, TreeNode10})
+        Dim TreeNode12 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("Standard Clearance")
+        Dim TreeNode13 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("Circuits")
+        Dim TreeNode14 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("VFR Clearances", New System.Windows.Forms.TreeNode() {TreeNode12, TreeNode13})
+        Me.ATCMenu = New System.Windows.Forms.TreeView()
+        Me.btnGenerateScript = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
-        'TreeView1
+        'ATCMenu
         '
-        Me.TreeView1.Location = New System.Drawing.Point(12, 12)
-        Me.TreeView1.Name = "TreeView1"
+        Me.ATCMenu.Location = New System.Drawing.Point(12, 12)
+        Me.ATCMenu.Name = "ATCMenu"
         TreeNode1.Name = "StandardClearance"
         TreeNode1.Text = "Standard Clearance"
         TreeNode2.Name = "AmendedRoute"
@@ -53,21 +60,44 @@ Partial Class ATCScripts
         TreeNode7.Text = "No SID Amended Route & FL"
         TreeNode8.Name = "IFRClearances"
         TreeNode8.Text = "IFR Clearances"
-        Me.TreeView1.Nodes.AddRange(New System.Windows.Forms.TreeNode() {TreeNode8})
-        Me.TreeView1.Size = New System.Drawing.Size(204, 426)
-        Me.TreeView1.TabIndex = 0
+        TreeNode9.Name = "TaxiIFR"
+        TreeNode9.Text = "IFR Taxi"
+        TreeNode10.Name = "TaxiVFR"
+        TreeNode10.Text = "VFRTaxi"
+        TreeNode11.Name = "TaxiInstructions"
+        TreeNode11.Text = "Taxi Instructions"
+        TreeNode12.Name = "VFRStandard"
+        TreeNode12.Text = "Standard Clearance"
+        TreeNode13.Name = "VFRCircuits"
+        TreeNode13.Text = "Circuits"
+        TreeNode14.Name = "VFRClearances"
+        TreeNode14.Text = "VFR Clearances"
+        Me.ATCMenu.Nodes.AddRange(New System.Windows.Forms.TreeNode() {TreeNode8, TreeNode11, TreeNode14})
+        Me.ATCMenu.Size = New System.Drawing.Size(204, 435)
+        Me.ATCMenu.TabIndex = 0
+        '
+        'btnGenerateScript
+        '
+        Me.btnGenerateScript.Location = New System.Drawing.Point(222, 147)
+        Me.btnGenerateScript.Name = "btnGenerateScript"
+        Me.btnGenerateScript.Size = New System.Drawing.Size(170, 83)
+        Me.btnGenerateScript.TabIndex = 1
+        Me.btnGenerateScript.Text = "Generate Script"
+        Me.btnGenerateScript.UseVisualStyleBackColor = True
         '
         'ATCScripts
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Controls.Add(Me.TreeView1)
+        Me.ClientSize = New System.Drawing.Size(404, 459)
+        Me.Controls.Add(Me.btnGenerateScript)
+        Me.Controls.Add(Me.ATCMenu)
         Me.Name = "ATCScripts"
         Me.Text = "ATC Scripts"
         Me.ResumeLayout(False)
 
     End Sub
 
-    Friend WithEvents TreeView1 As TreeView
+    Friend WithEvents ATCMenu As TreeView
+    Friend WithEvents btnGenerateScript As Button
 End Class
--- ATC-Scripts/ATCScripts.vb
+++ ATC-Scripts/ATCScripts.vb
@@ -1,5 +1,120 @@
 ﻿Public Class ATCScripts
-    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+    Private Sub ATCScripts_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim Menu As TreeView
+        Menu = ATCMenu
+
+        Menu.ExpandAll()
+
+    End Sub
+
+    Private Sub btnGenerateScript_Click(sender As Object, e As EventArgs) Handles btnGenerateScript.Click
+        Dim Menu As TreeView
+        Menu = ATCMenu
+        Dim nodeName As String
+        nodeName = Menu.SelectedNode.Name
+        If (nodeName = "StandardClearance") Then
+            '<Callsign>, cleared to <Destination> via <SID> departure, flight planned route. Depart runway <Runway>. Squawk <Squawk>.
+            Dim callsign, destination, sid, runway, squawk As String
+            callsign = InputBox("Enter callsign")
+            destination = InputBox("Enter destination")
+            sid = InputBox("Enter SID")
+            runway = InputBox("Enter runway")
+            squawk = InputBox("Enter squawk")
+            MsgBox(callsign + ", cleared to " + destination + " via " + sid + " departure, flight planned route. Depart runway " + runway + ". Squawk " + squawk)
+        ElseIf (nodeName = "AmendedRoute") Then
+            '<Callsign>, cleared to <desintation> via <sid> departure, amended route <sid>  flight planned route. Depart runway <runway>. Squawk <squawk>
+            Dim callsign, destination, sid, runway, squawk, amendedsid As String
+            callsign = InputBox("Enter callsign")
+            destination = InputBox("Enter destination")
+            sid = InputBox("Enter SID")
+            runway = InputBox("Enter runway")
+            squawk = InputBox("Enter squawk")
+            amendedsid = InputBox("Enter amended sid")
+            MsgBox(callsign + ", cleared to " + destination + " via " + sid + " departure, amended route " + amendedsid + ", flight planned route. Depart runway " + runway + ". Squawk " + squawk)
+        ElseIf (nodeName = "AmendedFL") Then
+            '<Callsign>, cleared to <desintation> via <sid> departure, flight planned route. Depart runway <runway>. Expect flight level <flight level> 1-0 minutes after departure. Squawk <squawk>
+            Dim callsign, destination, sid, runway, squawk, flightlevel As String
+            callsign = InputBox("Enter callsign")
+            destination = InputBox("Enter destination")
+            sid = InputBox("Enter SID")
+            runway = InputBox("Enter runway")
+            squawk = InputBox("Enter squawk")
+            flightlevel = InputBox("Enter amended flight level")
+            MsgBox(callsign + ", cleared to " + destination + " via " + sid + " departure, flight planned route. Depart runway " + runway + ". Expect flight level " + flightlevel + " 1-0 minutes after departure. Squawk " + squawk)
+        ElseIf (nodeName = "AmendedRouteFL") Then
+            '<Callsign>, cleared to <desintation> via <sid> departure, amended route <sid>  flight planned route. Depart runway <runway>. Expect flight level <flight level> 1-0 minutes after departure. Squawk <squawk>
+            Dim callsign, destination, sid, runway, squawk, flightlevel, amendedsid As String
+            callsign = InputBox("Enter callsign")
+            destination = InputBox("Enter destination")
+            sid = InputBox("Enter SID")
+            amendedsid = InputBox("Enter amended sid")
+            runway = InputBox("Enter runway")
+            flightlevel = InputBox("Enter amended flight level")
+            squawk = InputBox("Enter squawk")
+            MsgBox(callsign + ", cleared to " + destination + " via " + sid + " departure, amended route " + amendedsid + ", flight planned route. Depart runway " + runway + ". Expect flight level " + flightlevel + " 1-0 minutes after departure. Squawk " + squawk)
+        ElseIf (nodeName = "NoSID") Then
+            '<Callsign>, cleared to <destination>. Flight planned route. Depart runway <runway>. On departure, fly runway heading, Climb 7000. Squawk <squawk>
+            Dim callsign, destination, runway, squawk As String
+            callsign = InputBox("Enter callsign")
+            destination = InputBox("Enter destination")
+            runway = InputBox("Enter runway")
+            squawk = InputBox("Enter squawk")
+            MsgBox(callsign + ", cleared to " + destination + ". Flight planned route. Depart runway " + runway + ". On departure, fly runway heading, Climb 7000. Squawk " + squawk)
+        ElseIf (nodeName = "NoSIDAmended") Then
+            '<Callsign>, cleared to <destination>. Amended Route <SID>. Flight planned route. Depart runway <runway>. On departure, fly runway heading, Climb 7000. Squawk <squawk>
+            Dim callsign, destination, runway, squawk, amendedsid As String
+            callsign = InputBox("Enter callsign")
+            destination = InputBox("Enter destination")
+            amendedsid = InputBox("Enter amended sid")
+            runway = InputBox("Enter runway")
+            squawk = InputBox("Enter squawk")
+            MsgBox(callsign + ", cleared to " + destination + ". Amended route " + amendedsid + ". Flight planned route. Depart runway " + runway + ". On departure, fly runway heading, Climb 7000. Squawk " + squawk)
+        ElseIf (nodeName = "NoSIDFL") Then
+            '<Callsign>, cleared to <destination>. Amended Route <SID>. Flight planned route. Depart runway <runway>. Expect flight level <flight level> 1-0 minutes after departure. On departure, fly runway heading, Climb 7000. Squawk <squawk>
+            Dim callsign, destination, runway, squawk, amendedsid, flightlevel As String
+            callsign = InputBox("Enter callsign")
+            destination = InputBox("Enter destination")
+            amendedsid = InputBox("Enter amended sid")
+            runway = InputBox("Enter runway")
+            flightlevel = InputBox("Enter amended flight level")
+            squawk = InputBox("Enter squawk")
+            MsgBox(callsign + ", cleared to " + destination + ". Amended route " + amendedsid + ". Flight planned route. Depart runway " + runway + ". Expect flight level " + flightlevel + " 1-0 minutes after departure. On departure, fly runway heading, Climb 7000. Squawk " + squawk)
+        ElseIf (nodeName = "TaxiIFR") Then
+            '<Callsign>, Runway <runway>, Altimeter <setting>.Taxi apron your discretion <route>. Hold short runway <runway>.
+            Dim callsign, runway, setting, route, holdrnwy As String
+            callsign = InputBox("Enter callsign")
+            runway = InputBox("Enter runway")
+            setting = InputBox("Enter altimeter setting")
+            route = InputBox("Enter route")
+            holdrnwy = InputBox("Enter hold short runway")
+            MsgBox(callsign + ", Runway " + runway + ", Altimeter " + setting + ". Texi apron your discrection " + route + ". Hold short runway " + holdrnwy)
+        ElseIf (nodeName = "TaxiVFR") Then
+            '<Callsign>, Runway <runway>, Winds <winds>. Altimeter <setting>. Taxi via <route>. Hold short runway <runway>.
+            Dim callsign, runway, winds, setting, route, holdrnwy As String
+            callsign = InputBox("Enter callsign")
+            runway = InputBox("Enter runway")
+            winds = InputBox("Enter winds")
+            setting = InputBox("Enter altimeter setting")
+            route = InputBox("Enter route")
+            holdrnwy = InputBox("Enter hold short runway")
+            MsgBox(callsign + ", Runway " + runway + ", Winds " + winds + ", Altimeter " + setting + ". Texi apron your discrection " + route + ". Hold short runway " + holdrnwy)
+        ElseIf (nodeName = "VFRStandard") Then
+            '<Callsign>, VFR to <destination> Approved. Depart runway <runway>. Climb runway heading, not above 4500. Squawk <code>.
+            Dim callsign, destination, runway, code As String
+            callsign = InputBox("Enter callsign")
+            destination = InputBox("Enter destination")
+            runway = InputBox("Enter runway")
+            code = InputBox("Enter squawk")
+            MsgBox(callsign + ", VFR to " + destination + " approved. Depart runway " + runway + ". Climb runway heading, not above 4500. Squawk " + code)
+        ElseIf (nodeName = "VFRCircuits") Then
+            '<Callsign>, VFR Circuits Approved. Depart runway <runway>, <left/right> hand circuits, not above 4500. Squawk <code>.
+            Dim callsign, direction, runway, code As String
+            callsign = InputBox("Enter callsign")
+            runway = InputBox("Enter runway")
+            direction = InputBox("Enter left/right")
+            code = InputBox("Enter squawk")
+            MsgBox(callsign + ", VFR Circuits approved. Depart runway " + runway + ", " + direction + " hand circuits, not above 4500. Squawk " + code)
+        End If
 
     End Sub
 End Class
